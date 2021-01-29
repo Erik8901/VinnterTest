df9fe5cda1283d274e4760f49d2711837281ff2d
Erik <erikjohansson890@gmail.com>
2021-01-29 15:42:57 +0100

    banner 4 started

--- vinnter-test/src/components/BannerFour/BannerFour.js
+++ vinnter-test/src/components/BannerFour/BannerFour.js
@@ -7,7 +7,10 @@ import './styles/stylesBannerFour.css'
 function BannerFour() {
   return (
     <div className="BannerFour-div">
-       BannerFour
+        <span className="banner-thee-title">There are 3 main reasons behind successful IoT projects.</span>
+        <div>
+          <span>54% of businesses name the reason behind their projects’ success to be collaboration between IT and business.</span>
+        </div>
     </div>
   );
 }
