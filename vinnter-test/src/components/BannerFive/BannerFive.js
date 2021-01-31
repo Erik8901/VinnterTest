766592e4d3901e255417730ae7e4b49d4d8fd693
Erik <erikjohansson890@gmail.com>
2021-01-31 15:24:18 +0100

    banner five, almost done

--- vinnter-test/src/components/BannerFive/BannerFive.js
+++ vinnter-test/src/components/BannerFive/BannerFive.js
@@ -11,37 +11,37 @@ function BannerFive() {
     <div className="BannerFive-div">
       <span className="team-title">Our Team</span>
         <div className="team-photo-container">
-          <a >
+          <a className="photo-link">
             <div className="photo-container" >
                 <div class="hide">Namn Efternamn</div>
                 <img className="photo" src="https://vinnter.se/wp-content/uploads/2020/04/Adam-Gr%C3%B6nberg-1152x1536.jpg" />
                 </div>
           </a>
-          <a >
+          <a className="photo-link" >
             <div className="photo-container" >
                 <div class="hide">Namn Efternamn</div>
                 <img className="photo" src="https://vinnter.se/wp-content/uploads/2020/04/Adam-Gr%C3%B6nberg-1152x1536.jpg" />
                 </div>
           </a>
-          <a >
+          <a className="photo-link" >
             <div className="photo-container" >
                 <div class="hide">Namn Efternamn</div>
                 <img className="photo" src="https://vinnter.se/wp-content/uploads/2020/04/Adam-Gr%C3%B6nberg-1152x1536.jpg" />
                 </div>
           </a>
-          <a >
+          <a className="photo-link" >
             <div className="photo-container" >
                 <div class="hide">Namn Efternamn</div>
                 <img className="photo" src="https://vinnter.se/wp-content/uploads/2020/04/Adam-Gr%C3%B6nberg-1152x1536.jpg" />
                 </div>
           </a>
-          <a >
+          <a className="photo-link">
             <div className="photo-container" >
                 <div class="hide">Namn Efternamn</div>
                 <img className="photo" src="https://vinnter.se/wp-content/uploads/2020/04/Adam-Gr%C3%B6nberg-1152x1536.jpg" />
                 </div>
           </a>
-          <a >
+          <a className="photo-link">
             <div className="photo-container" >
                 <div class="hide">Namn Efternamn</div>
                 <img className="photo" src="https://vinnter.se/wp-content/uploads/2020/04/Adam-Gr%C3%B6nberg-1152x1536.jpg" />
